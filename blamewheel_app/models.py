1cf6c674cc3ea9e78781ea254ed8881b65920ab5
Rodrigo Menezes <rod333@gmail.com>
2012-06-08 01:52:59 +0000

    Made settings

--- blamewheel_app/models.py
+++ blamewheel_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class BlameWheel(models.Model):
+    blamewheel_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, related_name="BlamewheelUser")
+    name = models.CharField(max_length=200)
+    rigged = models.ForeignKey('Blame')
+
+class Blame(models.Model):
+    blame_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    color = models.CharField(max_length=6)
+    size = models.IntegerField()
